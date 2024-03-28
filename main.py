bba9e35289e47e12cd9a05fc2888d4ee7c07db0c
pab-h <dev.pab.2020@gmail.com>
2024-03-28 09:51:40 -0300

    feat: main func to run code script

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from boole.Interpreter import Interpreter
+
 def main() -> None:
-    pass
+    interpreter = Interpreter()    
+
+    while True:
+        try: 
+            text = input(">>>")
+            print(interpreter.eval(text))
+
+        except EOFError:
+            print("\n\nBye bye!\n")
+            break
+
+if __name__ == "__main__":
+    main()
