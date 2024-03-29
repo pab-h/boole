1ebb7a3622d46983b12c2584eb8ed3eb82d68310
pab-h <dev.pab.2020@gmail.com>
2024-03-29 11:33:50 -0300

    feat: improve AST and AST nodes

--- boole/analysis/ast.py
+++ boole/analysis/ast.py
@@ -1,9 +1,32 @@
 from boole.analysis.token import Token
 
+from enum import Enum
+from enum import auto
+
 from typing import Optional
 
+class ASTNodeTypes(Enum):
+    ABSTRACT = auto()
+    BINARYOPERATOR = auto()
+    LOGIC = auto()
+
 class AST(object):
-    def __init__(self, value: Token) -> None:
-        self.value = value
-        self.left: Optional[Token] = None
-        self.right: Optional[Token] = None
+    def __init__(self, token: Token) -> None:
+        self.token = token
+        self.type = ASTNodeTypes.ABSTRACT
+
+    def __repr__(self) -> str:
+        return f"AST[token = { self.token }, type = { self.type.name }]"
+
+class BinaryOperator(AST):
+    def __init__(self, token: Token) -> None:
+        super().__init__(token)
+        self.type = ASTNodeTypes.BINARYOPERATOR
+        self.left: Optional[AST] = None 
+        self.right: Optional[AST] = None 
+
+class Logic(AST):
+    def __init__(self, token: Token) -> None:
+        super().__init__(token)
+        self.type = ASTNodeTypes.LOGIC
+        self.value = token.value
