ef57a00061dca4908df265976e06914205ab1749
pab-h <dev.pab.2020@gmail.com>
2024-03-29 11:35:37 -0300

    feat: adaptation to walking in AST

--- boole/Interpreter.py
+++ boole/Interpreter.py
@@ -1,13 +1,21 @@
 from boole.analysis.parser import Parser
+
 from boole.analysis.ast import AST
+from boole.analysis.ast import BinaryOperator
+from boole.analysis.ast import Logic
+from boole.analysis.ast import ASTNodeTypes
+
 from boole.analysis.token import TokenTypes
+
+class InterpreterError(Exception):
+    pass
 
 class Interpreter(object):
     def __init__(self) -> None:
         self.parser = Parser()
 
-    def visit(self, node: AST) -> bool:
-        token = node.value
+    def visitBinaryOperator(self, node: BinaryOperator) -> bool:
+        token = node.token
 
         if token.type == TokenTypes.AND:
             return self.visit(node.left) and self.visit(node.right)
@@ -15,8 +23,20 @@ class Interpreter(object):
         if token.type == TokenTypes.OR:
             return self.visit(node.left) or self.visit(node.right)
         
-        if token.type == TokenTypes.LOGIC:
-            return token.value
+        raise InterpreterError("invalid binary operator")
+
+    def visitLogic(self, node: Logic) -> bool:
+        return node.value
+
+    def visit(self, node: AST) -> bool:
+
+        if node.type == ASTNodeTypes.BINARYOPERATOR:
+            return self.visitBinaryOperator(node)
+        
+        if node.type == ASTNodeTypes.LOGIC:
+            return self.visitLogic(node)
+
+        raise InterpreterError("Invalid AST node")
 
     def eval(self, text): 
         ast = self.parser.parse(text)
