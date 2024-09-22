489fc277a9dc88a954eadf77606f0d0d16feeed7
pab-h <dev.pab.2020@gmail.com>
2024-09-22 12:20:26 -0300

    feat: lexer

---
+++ boole/analysis/lexer.py
@@ -0,0 +1,247 @@
+from boole.analysis.token import *
+
+def advance(method):
+    def wrapper(self, *args, **kwargs):
+        result = method(self, *args, **kwargs)
+        self.index += 1
+        
+        return result
+    
+    return wrapper
+
+class Lexer:
+    def __init__(self) -> None:
+        self.index = 0
+        self.buffer = ""
+
+    @property
+    def hasNextToken(self) -> bool:
+        return len(self.buffer) > self.index
+    
+    @property
+    def currentChar(self) -> str:
+        return self.buffer[self.index]
+
+    def identifier(self) -> Token:
+        identifier = ""
+
+        while self.hasNextToken and self.currentChar.isalpha():
+            identifier += self.currentChar
+            self.index += 1
+
+        return Token(
+            identifier,
+            TokenTypes.IDENTIFIER
+        )
+    
+    def assign(self) -> Token:
+        assign = self.currentChar
+        self.index += 1
+
+        if self.hasNextToken and self.currentChar == "=":
+            assign += self.currentChar
+            self.index += 1
+
+        if assign != ":=":
+            raise MalformedTokenError(
+                f"Malformed attribution token: { assign }"
+            )
+        
+        return Token(
+            assign,
+            TokenTypes.ASSIGN
+        )
+    
+    def implication(self): 
+        implication = self.currentChar
+        self.index += 1
+
+        if self.hasNextToken and self.currentChar == ">":
+            implication += self.currentChar
+            self.index += 1
+
+        if implication != "->":
+            raise MalformedTokenError(
+                f"Malformed attribution token: { implication }"
+            )
+        
+        return Token(
+            implication,
+            TokenTypes.IMPLICATION
+        )
+    
+    def biimplication(self): 
+        biimplication = self.currentChar
+        self.index += 1
+
+        if self.hasNextToken and self.currentChar == "-":
+            biimplication += self.currentChar
+            self.index += 1
+
+        if self.hasNextToken and self.currentChar == ">":
+            biimplication += self.currentChar
+            self.index += 1
+
+        if biimplication != "<->":
+            raise MalformedTokenError(
+                f"Malformed attribution token: { biimplication }"
+            )
+        
+        return Token(
+            biimplication,
+            TokenTypes.IMPLICATION
+        )
+
+    @advance
+    def out(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.OUT
+        )
+
+    @advance
+    def bit(self) -> Token:
+        return Token(
+            self.currentChar == "1",
+            TokenTypes.BIT
+        )
+
+    @advance
+    def _and(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.AND
+        )
+    
+    @advance
+    def _or(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.OR
+        )
+    
+    @advance
+    def xor(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.XOR
+        )
+    
+    @advance
+    def _not(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.NOT
+        )
+
+    @advance
+    def leftParent(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.LEFT_PARENT
+        )
+    
+    @advance
+    def rightParent(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.RIGHT_PARENT
+        )
+    
+    @advance
+    def leftBrackets(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.LEFT_BRACKETS
+        )
+    
+    @advance
+    def rightBrackets(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.RIGHT_BRACKETS
+        )
+
+    @advance
+    def comma(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.COMMA
+        )
+    
+    @advance
+    def whitespace(self) -> Token:
+        return Token(
+            self.currentChar,
+            TokenTypes.WHITESPACE
+        )
+
+
+    def nextToken(self) -> Token:
+        if self.currentChar == ">":
+            return self.out()
+
+        if self.currentChar in "01":
+            return self.bit()
+        
+        if self.currentChar == "*":
+            return self._and()
+        
+        if self.currentChar == "+":
+            return self._or()
+        
+        if self.currentChar == "^":
+            return self.xor()
+        
+        if self.currentChar == "!":
+            return self._not()
+        
+        if self.currentChar == "(":
+            return self.leftParent()
+        
+        if self.currentChar == ")":
+            return self.rightParent()
+        
+        if self.currentChar == "[":
+            return self.leftBrackets()
+        
+        if self.currentChar == "]":
+            return self.rightBrackets()
+        
+        if self.currentChar == ",":
+            return self.comma()
+
+        if self.currentChar == "-":
+            return self.implication()
+        
+        if self.currentChar == "<":
+            return self.biimplication()
+        
+        if self.currentChar == ":":
+            return self.assign()
+        
+        if self.currentChar.isalpha():
+            return self.identifier()
+
+        if self.currentChar.isspace():
+            return self.whitespace()
+
+        raise UnrecognizedTokenError(
+            f"Unrecognized token: { self.currentChar }"
+        )
+
+    def lex(self, buffer: str) -> list[Token]:
+        self.buffer = buffer
+        self.index = 0
+
+        tokens = []
+
+        while self.hasNextToken:
+            token = self.nextToken()
+
+            if token.type == TokenTypes.WHITESPACE:
+                continue
+
+            tokens.append(token)
+
+        return tokens
---
+++ boole/analysis/token.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from enum import auto
+
+class UnrecognizedTokenError(Exception):
+    pass
+
+class MalformedTokenError(Exception):
+    pass
+
+class TokenTypes(Enum):
+    BIT = auto()
+    IDENTIFIER = auto()
+
+    OR = auto()
+    AND = auto()
+    NOT = auto()
+    XOR = auto()
+    IMPLICATION = auto()
+    BIIMPLICATION = auto()
+    
+    LEFT_PARENT = auto()
+    RIGHT_PARENT = auto()
+    LEFT_BRACKETS = auto()
+    RIGHT_BRACKETS = auto()
+    
+    ASSIGN = auto()
+    COMMA = auto()
+    
+    OUT = auto()
+
+    WHITESPACE = auto()
+
+class Token:
+    def __init__(self, value, _type: TokenTypes) -> None:
+        self.value = value
+        self.type = _type
+
+    def __repr__(self) -> str:
+        return f"Token[value = \"{self.value}\", type = {self.type.name}]"
