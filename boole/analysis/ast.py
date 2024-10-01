d1c3ca4d52f0d81ce23fc52ff8c844a9989e392b
pab-h <dev.pab.2020@gmail.com>
2024-09-30 22:50:37 -0300

    feat: abstract syntaxe tree

---
+++ boole/analysis/ast.py
@@ -0,0 +1,102 @@
+from boole.analysis.token import Token
+
+from enum import Enum
+from enum import auto
+
+class ASTTypes(Enum):
+    ABSTRACT = auto()
+
+    COMPOUND = auto()
+    ASSIGN = auto()
+    FUNCTION_DEFINITION = auto()
+    FUNCTION_CALL = auto()
+
+    UNARY_OPERATION = auto()
+    BINARY_OPERATION = auto()
+
+    BIT = auto()
+    STREAM = auto()
+    IDENTIFIER = auto()
+
+class AST:
+    def __init__(self) -> None:
+        self.type = ASTTypes.ABSTRACT
+
+class UnaryOperationNode(AST): 
+    def __init__(self, operator: Token, value: AST) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.UNARY_OPERATION
+
+        self.operator = operator
+        self.value = value
+
+class BinaryOperationNode(AST):
+    def __init__(self, operator: Token, left: AST, right: AST) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.BINARY_OPERATION
+
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+class AssignNode(AST):
+    def __init__(self, identifier: Token, value: AST) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.ASSIGN
+
+        self.identifier = identifier
+        self.value = value
+
+class CompoundNode(AST):
+    def __init__(self, children: list[AST]) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.COMPOUND
+
+        self.children = children
+
+class FunctionDefinitionNode(AST):
+    def __init__(self, identifier: Token, params: list[Token], body: AST) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.FUNCTION_DEFINITION
+
+        self.identifier = identifier
+        self.params = params
+        self.body = body
+
+class FunctionCallNode(AST):
+    def __init__(self, identifier: Token, arguments: list[AST]) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.FUNCTION_CALL
+
+        self.identifier = identifier
+        self.arguments = arguments
+
+class BitNode(AST):
+    def __init__(self, value: Token) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.BIT
+
+        self.value = value
+
+class StreamNode(AST):
+    def __init__(self, identifier: Token, bits: list[Token]) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.STREAM
+
+        self.identifier = identifier
+        self.bits = bits
+
+class IdentifierNode(AST):
+    def __init__(self, identifier: Token) -> None:
+        super().__init__()
+
+        self.type = ASTTypes.IDENTIFIER
+        self.identifier = identifier
